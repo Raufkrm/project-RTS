20c825c3fb309e231002dcd156fb0d06f0affc9b
ErenAtes01 <01.ates.eren@gmail.com>
2025-10-21 14:18:15 +0200

    Planet has a bit more LOD

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -1,12 +1,12 @@
-use bevy::prelude::*;
 use crate::app::AppState;
+
 use crate::game::world::planet::{spawn_random_planet_inner, PlanetParams};
-use crate::game::world::terrain::MapSettings;
 use crate::game::world::sampling::FlatSamplerRes;
-
-pub mod world;
+use crate::game::world::terrain::MapSettings;
+use bevy::prelude::*;
 pub mod ui;
-
+pub mod world;
+use crate::game::world::planet::{auto_clip_planes, update_planet_lod};
 #[derive(Component)]
 struct InGameRoot;
 
@@ -18,6 +18,10 @@ impl Plugin for GamePlugin {
             .init_resource::<MapSettings>()
             .init_resource::<PlanetParams>()
             .init_resource::<FlatSamplerRes>()
+            .add_systems(
+                Update,
+                (update_planet_lod, auto_clip_planes).run_if(in_state(AppState::InGame)),
+            )
             // Your dev UI etc.
             .add_plugins(ui::dev_panel::DevPanelPlugin)
             // When we enter InGame, set up the world
